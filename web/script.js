dcda766ca766c62cd439ade5e1c3104e233b971a
joel <smx.joelpl@gmail.com>
2020-05-21 11:01:16 +0200

    producto

--- web/script.js
+++ web/script.js
@@ -3,50 +3,34 @@ var navegacio = null
 var popups = null
 var seccioBackofficeUsuaris = null
 var seccioFrontendProductes = null
-
+var seccioFrontendProducte = null
 // Aquesta funció s'inicia al carregar la pàgina
 async function inicia () {
-
-    // Iniciem els objectes globals
-    login = new ObjLogin()
-    navegacio = new ObjNavegacio()
-    popups = new ObjPopups()
-    seccioBackofficeUsuaris = new ObjSeccioBackofficeUsuaris()
-    seccioFrontendProductes = new ObjSeccioFrontendProductes()
-
-    // Inicia les funcions de navegació HTML5
-    navegacio.inicia()
-
-    // Fem que els botons de navegació endavant i endarrera mostrin el canvi de secció
-    window.onpopstate = function (evt) {
-        if (evt.state === null) {
-            navegacio.mostraSeccio('frontendHome')
-        } else {
-            navegacio.mostraSeccio(evt.state.html)
-        }
-    }
-
-    // Si tenim guardat un usuari i un token intentem identificar-lo
-    await login.autenticaAmbToken()
+// Iniciem els objectes globals
+login = new ObjLogin()
+navegacio = new ObjNavegacio()
+popups = new ObjPopups()
+seccioBackofficeUsuaris = new ObjSeccioBackofficeUsuaris()
+seccioFrontendProductes = new ObjSeccioFrontendProductes()
+seccioFrontendProducte = new ObjSeccioFrontendProducte()
+// Inicia les funcions de navegació HTML5
+navegacio.inicia()
+// Fem que els botons de navegació endavant i endarrera mostrin el canvi de secció
+window.onpopstate = function (evt) {
+if (evt.state === null) {
+navegacio.mostraSeccio('frontendHome')
+} else {
+navegacio.mostraSeccio(evt.state.html)
 }
-
-function iniciaSeccio(seccio) {
-    switch(seccio) {
-    case 'frontendProductes': seccioFrontendProductes.iniciaSeccio(); break
-    case 'backofficeUsuaris': seccioBackofficeUsuaris.iniciaSeccio(); break
-    default:
-    }
 }
-
-
-function mostraMenu () {
-    let ref = document.getElementById('frontendMenuMobil'),
-        estilo = window.getComputedStyle(ref, ''),
-        alto = parseInt(estilo.getPropertyValue('height'), 10)
-
-    if (alto > 25) {
-        ref.style.height = '25px'
-    } else {
-        ref.style.height = 'auto'
-    }
+// Si tenim guardat un usuari i un token intentem identificar-lo
+await login.autenticaAmbToken()
+}
+function iniciaSeccio(seccio, id) {
+switch(seccio) {
+case 'frontendProductes': seccioFrontendProductes.iniciaSeccio(); break
+case 'frontendProducte':  seccioFrontendProducte.iniciaSeccio(id); break
+case 'backofficeUsuaris': seccioBackofficeUsuaris.iniciaSeccio(); break
+default:
+}
 }
