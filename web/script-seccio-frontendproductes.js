26b0f5127205402be4397c637d92a5856e9295be
joel <smx.joelpl@gmail.com>
2020-05-21 11:08:44 +0200

    producto

--- web/script-seccio-frontendproductes.js
+++ web/script-seccio-frontendproductes.js
@@ -23,44 +23,6 @@ class ObjSeccioFrontendProductes {
         if (objRebut.resultat === 'ok') {
             for (cntProducte = 0; cntProducte < objRebut.missatge.length; cntProducte = cntProducte + 1) {
                 valor = objRebut.missatge[cntProducte]
-                codiHTML = codiHTML + '<div align="center"><img src="' + valor.imatge + '" width="200" /></div>'
-                codiHTML = codiHTML + '<h3 class="letraproductos">' + valor.nom +'</h3>'
-                codiHTML = codiHTML + '<div class="letraproductos">' + valor.descripcio +'</div>'
-                codiHTML = codiHTML + '<div class="letraproductos">' + valor.preu +' €</div>'
-                codiHTML = codiHTML + '<hr/>'
-            }
-        }
-
-        // Amaguem la càrrega i mostrem el llistat de productes
-        refContinguts.innerHTML = codiHTML
-        refContinguts.style.display = 'flex'
-        refLoading.style.display = 'none'
-    }
-}
-class ObjSeccioFrontendProductes {
-
-    constructor () {
-    }
-
-    async iniciaSeccio () {
-        let refLoading = document.getElementById('productesLoading'),
-            refContinguts = document.getElementById('productesContinguts'),
-            objRebut = null,
-            valor = null,
-            codiHTML = '',
-            cntProducte = 0
-
-        // Amaguem els continguts actuals i mostrem la càrrega
-        refContinguts.style.display = 'none'
-        refLoading.style.display = 'flex'
-
-        // Demanem el llistat de productes al servidor
-        objRebut = await promiseCallServer('POST', '/call/llistatProductes', {})
-        console.log(objRebut)
-        // Transformem l'objecte rebut en codi HTML
-        if (objRebut.resultat === 'ok') {
-            for (cntProducte = 0; cntProducte < objRebut.missatge.length; cntProducte = cntProducte + 1) {
-                valor = objRebut.missatge[cntProducte]
                 codiHTML = codiHTML + '<div class="detall" onclick=\'navegacio.canviaSeccio("frontendProducte&' + cntProducte + '")\'>'
                 codiHTML = codiHTML + '<h3 class="letraproductos">' + valor.nom +'</h3>'
                 codiHTML = codiHTML + '<div class="letraproductos">' + valor.descripcio +'</div>'
